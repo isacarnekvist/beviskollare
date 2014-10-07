4b587b08a87ff0d3616d7edbfdca0c609c4d3f30
isacarnekvist <isac.arnekvist@gmail.com>
2014-10-07 13:24:17 +0200

    Minor updates

--- ny.pl
+++ ny.pl
@@ -13,27 +13,27 @@ checkPremises([[[_,_,assumption]|BT]|T], _) 			:-	checkPremises(BT, false),
                                                             checkPremises(T, false).
 checkPremises([],_).
 
-checkEndLineNumber([_|T], Last)                         :-	checkEndLineNumber(T, Last).
+checkEndLineNumber([_|T], Last)                         :-  checkEndLineNumber(T, Last).
 checkEndLineNumber([[Last,_,_]|[]], Last).
 
 % Last line!
-valid_proof(Prems, G1, Stop, [[Stop,G2,F]|[]], Proved) 	:-	!, checkLine(Prems, [Stop,G2,F], Proved),
-															G1 == G2.
+valid_proof(Prems, G1, Stop, [[Stop,G2,F]|[]], Proved) 	:-  !, checkLine(Prems, [Stop,G2,F], Proved),
+                                                            G1 == G2.
 % Otherwise, usual line
-valid_proof(Prems, G, Stop, [[L,D,R]|T], Proved)		:- 	!, checkLine(Prems, [L, D, R], Proved), % Regeln behövs inte i Proved
-															valid_proof(Prems, G, Stop, T, [[L,D]|Proved]).
+valid_proof(Prems, G, Stop, [[L,D,R]|T], Proved)        :-  !, checkLine(Prems, [L, D, R], Proved), % Regeln behövs inte i Proved
+                                                            valid_proof(Prems, G, Stop, T, [[L,D]|Proved]).
 % Box, add to proven, prove when asked for
-valid_proof(Prems, G, Stop, [[[L,D,assumption]|BT]|T], Proved)	:-	valid_proof(Prems, G, Stop, T, [[[L,D,assumption]|BT]|Proved]).
+valid_proof(Prems, G, Stop, [[[L,D,assumption]|BT]|T], Proved   :-  valid_proof(Prems, G, Stop, T, [[[L,D,assumption]|BT]|Proved]).
 
-checkLine(Prems, [_, D, premise], _)					:-	member(D, Prems), !.
-checkLine(_	, [_, B, impel(X,Y)], Proved)				:-	member([X, imp(A,B)], Proved), member([Y, A], Proved), !.
-checkLine(_		, [_, neg(A), mt(X,Y)], Proved)			:-	member([X, imp(A,B)], Proved), member([Y, neg(B)], Proved), !.
-checkLine(_, [_, A, negnegel(X)], Proved)				:-	member([X, neg(neg(A))], Proved), !.
-checkLine(_	, [_, _, assumption], _).					
-checkLine(_, [_, imp(A,B), impint(X,Y)], [Box|T])       :-	[[X,A,assumption]|_] = Box, valid_proof([], B, Y, Box, T).
+checkLine(Prems, [_, D, premise], _)                    :-  member(D, Prems), !.
+checkLine(_, [_, B, impel(X,Y)], Proved)                :-  member([X, imp(A,B)], Proved), member([Y, A], Proved), !.
+checkLine(_, [_, neg(A), mt(X,Y)], Proved)              :-  member([X, imp(A,B)], Proved), member([Y, neg(B)], Proved), !.
+checkLine(_, [_, A, negnegel(X)], Proved)               :-  member([X, neg(neg(A))], Proved), !.
+checkLine(_, [_, _, assumption], _).					
+checkLine(_, [_, imp(A,B), impint(X,Y)], [Box|T])       :-  [[X,A,assumption]|_] = Box, valid_proof([], B, Y, Box, T).
 
 
-readProof(InputFileName, Prems, Goal, Proof) 			:- 	see(InputFileName),
+readProof(InputFileName, Prems, Goal, Proof)            :-  see(InputFileName),
                                                             read(Prems), read(Goal), read(Proof),
                                                             seen.
 
